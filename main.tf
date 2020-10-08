be70c6152b6780f2a12859bf9f06e293d0d2c1a0
gopal das <gopal1409@gmail.com>
2020-10-08 05:23:08 -0600

    vnet in azure

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ resource "azurerm_resource_group" "myterraform" {
         environment = "Terraform Metlife"
     }
 }
+
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name = "mvVnet"
+    address_space = ["10.0.0.0/16"]
+    location = "eastus"
+    resource_group_name = azurerm_resource_group.myterraform.name
+    tags = {
+        environment = "Terraform Metlife"
+    }
+}
