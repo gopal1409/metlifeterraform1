b7558ea693b87b8f50d1678d0c509e2789daff4d
gopal das <gopal1409@gmail.com>
2020-10-08 04:36:07 -0600

    resource group

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ provider "azurerm" {
     version = "~>2.0"
     features {}
 }
+
+# we will create a resource group
+resource "azurerm_resource_group" "myterraform" {
+    name = "myResourceGroup"
+    location = "eastus"
+    tags = {
+        environment = "Terraform Metlife"
+    }
+}
