46c3b930cd214820271f53eeb9374ddc00a25824
gopal das <gopal1409@gmail.com>
2020-10-09 04:24:29 -0600

    virtual machine

--- main.tf
+++ main.tf
@@ -103,7 +103,48 @@ resource "azurerm_storage_account" "mystorageaccount" {
     }
 }
 
+resource "tls_private_key" "example_ssh" {
+    algorithm = "RSA"
+    rsa_bits = 4096
+}
 
+output "tls_private_key" { value = tls_private_key.example_ssh.private_key_pem }
+
+resource "azurerm_linux_virtual_machine" "myterraformvm"{
+    name = "myVM"
+    location = "eastus"
+    resource_group_name = azurerm_resource_group.myterraform.name
+    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+    size = "Standard_DS1_V2"
+
+    os_disk {
+        name = "myOSDisk"
+        caching = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+    }
+    source_image_reference {
+        publisher = "Canonical"
+        offer = "UbuntuServer"
+        sku = "18.04-LTS"
+        version = "latest"
+    }
+
+    computer_name = "myvm"
+    admin_username = "gopal"
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username = "gopal"
+        public_key = tls_private_key.example_ssh.public_key_openssh
+    }
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+    }
+    tags = {
+        environment = "Terraform Metlife"
+    }
+
+}
 
 
 
