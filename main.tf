5b05d48c36e63c41c09097e6dcaf3bbdbc42c208
gopal das <gopal1409@gmail.com>
2020-10-09 02:02:43 -0600

    subnet

--- main.tf
+++ main.tf
@@ -11,13 +11,21 @@ resource "azurerm_resource_group" "myterraform" {
         environment = "Terraform Metlife"
     }
 }
-
+# creating a virtual network 
 resource "azurerm_virtual_network" "myterraformnetwork" {
     name = "mvVnet"
-    address_space = ["10.0.0.0/16"]
+    address_space = ["1.0.0.0/16"]
     location = "eastus"
     resource_group_name = azurerm_resource_group.myterraform.name
     tags = {
         environment = "Terraform Metlife"
     }
+}
+
+resource "azurerm_subnet" "myterraformsubnet" {
+    name = "mysubnet"
+    resource_group_name = azurerm_resource_group.myterraform.name
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    address_prefixes = ["1.0.2.0/24"]
+
 }
